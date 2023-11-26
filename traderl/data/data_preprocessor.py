64c0c1012c67268639ea6c4360a51ee8f2cc1a5b
nagi <komoootv@gmail.com>
2023-11-26 16:23:02 +0900

    [update]  ログ情報を追加

--- traderl/data/data_preprocessor.py
+++ traderl/data/data_preprocessor.py
@@ -6,154 +6,88 @@ import numpy as np
 import ta
 from sklearn.preprocessing import RobustScaler
 import argparse
+import logging
+import time
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
 
 
 def load_data(remote: bool, symbol: str = None) -> pd.DataFrame:
-    """
-    Load data from a CSV file or a remote source.
-
-    Parameters:
-    remote (bool): If True, load data from a remote source. If False, load data from a local CSV file.
-    symbol (str, optional): The symbol of the data to load from the remote source. Required if remote is True.
-
-    Returns:
-    pd.DataFrame: The loaded data.
-    """
+    start_time = time.time()
+    logging.debug(f"Loading data. Remote: {remote}, Symbol: {symbol}")
     if remote:
         if symbol is None:
             raise ValueError("Symbol are required when loading data from a remote source.")
-        # Load data from a remote source, Yahoo in this case.
         data = pdr.get_data_yahoo(symbol)
     else:
         file_path = symbol
         if file_path is None:
             raise ValueError("File path is required when loading data from a local file.")
-        # Load data from a local CSV file.
         data = pd.read_csv(file_path)
-
+    logging.debug(f"Data loaded in {time.time() - start_time} seconds.")
     return data
 
 
 def preprocess_data(data: pd.DataFrame, symbol: str, financial_type: str) -> pd.DataFrame:
-    """
-    Preprocess data.
-
-    Parameters:
-    data (pd.DataFrame): The data to preprocess.
-    symbol (str): The symbol of the currency pair.
-    financial_type (str): The type of financial data (e.g., 'FX', 'Stock').
-
-    Returns:
-    pd.DataFrame: The preprocessed data.
-    """
-    # Convert column names to lowercase.
+    logging.debug(f"Preprocessing data. Symbol: {symbol}, Financial type: {financial_type}")
     data.columns = data.columns.str.lower()
-
-    # Determine the pip scale based on the currency pair and financial type.
     if financial_type == 'FX':
         pip_scale = 100000 if 'JPY' not in symbol else 1000
-        # Scale the data by the pip value.
         data = data * pip_scale
-
-    # Add technical indicators.
     data = add_technical_indicators(data)
-
+    logging.debug(f"Data shape after preprocessing: {data.shape}")
     return data
 
 
 def add_technical_indicators(data: pd.DataFrame) -> pd.DataFrame:
-    """
-    Add technical indicators to the data.
-
-    Parameters:
-    data (pd.DataFrame): The data to add the technical indicators to.
-
-    Returns:
-    pd.DataFrame: The data with the added technical indicators.
-    """
-
-    # Relative Strength Index
+    logging.debug("Adding technical indicators.")
     data['rsi'] = ta.momentum.rsi(data['Close'])
-
-    # Moving Average Convergence Divergence
     data['macd'] = ta.trend.MACD(data.close).macd_diff()
-
-    # Short-term signal: Crossover of 5-day and 10-day EMA (Exponential Moving Average)
     data['ema_5'] = ta.trend.ema_indicator(data['Close'], window=5)
     data['ema_10'] = ta.trend.ema_indicator(data['Close'], window=10)
     data['ema_5_10_crossover'] = np.where(data['ema_5'] > data['ema_10'], 1, -1)
-
-    # Long-term signal: 200-day EMA
     data['ema_200'] = ta.trend.ema_indicator(data['Close'], window=200)
-
-    # Bollinger Bands
     BollingerBands = ta.volatility.BollingerBands(data['Close'])
     data['b_pband'] = BollingerBands.bollinger_pband()
     data['b_wband'] = BollingerBands.bollinger_wband()
-
-    # Average True Range
     data['atr'] = ta.volatility.average_true_range(data['High'], data['Low'], data['Close'])
-
+    logging.debug(f"Data shape after adding technical indicators: {data.shape}")
     return data
 
 
 def scale_and_split_data(data: pd.DataFrame, window_size: int = 30) -> np.array:
-    """
-    Scale and split the data into chunks of a specified window size.
-
-    Parameters:
-    data (pd.DataFrame): The data to scale and split.
-    window_size (int, optional): The size of the window to split the data into. Default is 30.
-
-    Returns:
-    np.array: The scaled and split data.
-    """
-    # Scale the data.
+    logging.debug(f"Scaling and splitting data. Window size: {window_size}")
     scaler = RobustScaler()
     data_scaled = scaler.fit_transform(data)
-
-    # Select specific columns for the scaled data
     data_scaled = np.array(data_scaled[["ema_5_10_crossover", "ema_200", "rsi", "macd", "b_pband", "b_wband", "atr"]])
-
-    # Split the data into chunks of the specified window size.
     data_split = []
     for i in range(len(data_scaled) - window_size + 1):
         data_split.append(data_scaled[i:i + window_size])
-
+    logging.debug(f"Data shape after scaling and splitting: {data.shape}")
     return np.array(data_split)
 
 
 def main(remote: bool, folder_path: str = None, symbols: list = None, financial_type: str = None):
-    """
-    Main function.
-
-    Returns:
-    None
-    """
+    logging.debug(
+        f"Running main function. Remote: {remote}, Folder path: {folder_path}, Symbols: {symbols}, Financial type: {financial_type}")
     market_datas = []
     state_datas = []
-
     if not remote:
         symbols = glob.glob(folder_path + '/*.csv')
-
     for symbol in symbols:
         data = load_data(remote, symbol=symbol)
         data = preprocess_data(data, symbol, financial_type)
         market_datas.append(data)
         state_datas.append(scale_and_split_data(data))
-
-    # Reshape state_datas
     state_datas = np.stack(state_datas, axis=0)
-
-    # Split market_datas into OHLC and reshape
     opens = np.stack([data['open'].values for data in market_datas], axis=0)
     highs = np.stack([data['high'].values for data in market_datas], axis=0)
     lows = np.stack([data['low'].values for data in market_datas], axis=0)
     closes = np.stack([data['close'].values for data in market_datas], axis=0)
     atrs = np.stack([data['atr'].values for data in market_datas], axis=0)
-
-    # Save state_datas and OHLC data in .npz format
     np.savez('data.npz', state=state_datas, open=opens, high=highs, low=lows, close=closes, atr=atrs)
+    logging.debug("Main function completed.")
 
 
 if __name__ == "__main__":
