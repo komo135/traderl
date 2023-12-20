03a89bd0ada7a160ecfa4bf5318dc6ecaf290fd3
nagi <komoootv@gmail.com>
2023-12-20 18:16:47 +0900

    Dropout　追加

--- traderl/model/build.py
+++ traderl/model/build.py
@@ -27,6 +27,7 @@ class QModel(torch.nn.Module):
         fc = torch.nn.Sequential(
             torch.nn.Linear(input_channels, 512, device=self.device),
             torch.nn.ELU(),
+            torch.nn.Dropout(0.4),
             torch.nn.Linear(512, self.action_space, device=self.device),
         )
         return fc
@@ -57,6 +58,7 @@ class ActorModel(torch.nn.Module):
         fc = torch.nn.Sequential(
             torch.nn.Linear(input_channels, 512, device=self.device),
             torch.nn.ELU(),
+            torch.nn.Dropout(0.5),
             torch.nn.Linear(512, self.action_space, device=self.device),
             torch.nn.Tanh()
         )
