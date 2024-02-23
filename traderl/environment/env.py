Fix stop loss calculation in Env class

traderl/environment/env.py
@@ -276,10 +276,10 @@ class Env:
 
                 event = None
                 if lower_pip <= -stop_loss:
-                    if pip >= 0:
+                    if ((open[i] - open[old_i]) * action - self.spread) >= 0:
                         print(f"pip: {pip}, lower_pip: {lower_pip} now_lower_pip:"
                               f"{now_lower_pip}, stop_loss: {stop_loss}, position_size: {position_size}, action: {action}"
-                              f"symbol: {self.symbol}, old_i: {old_i}, i: {i}, start_index: {start_index}, end_index: {end_index}")
+                              f" symbol: {self.symbol}, old_i: {old_i}, i: {i}, start_index: {start_index}, end_index: {end_index}")
 
                     pip = -stop_loss
                     is_stop = True
@@ -288,7 +288,6 @@ class Env:
                     pip = take_profit
                     is_stop = True
                     event = "take profit"
-
                     add_hit_point = np.round(pip / stop_loss, 3)
                 elif trade_length >= 50:
                     is_stop = True